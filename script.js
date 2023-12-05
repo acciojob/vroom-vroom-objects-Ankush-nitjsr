97508007843ff57a66986a801a6ac295e7be85ae
Ankush <125112667+Ankush-nitjsr@users.noreply.github.com>
2023-12-05 16:42:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,22 @@
 // Complete the js code
-function Car(make, model) {}
+function Car(make, model) {
+		this._make = make;
+		this._model = model;
+}
+Object.prototype.getMakeModel = function() {
+	return `${this._make} and ${this._model}`;
+}
 
-function SportsCar(make, model, topSpeed) {}
+function SportsCar(make, model, topSpeed) {
+	Car.call(this, make, model);
+	this._topSpeed = topSpeed;
+}
+
+SportsCar.prototype = Object.create(Car.prototype);
+
+SportsCar.prototype.getTopSpeed = function() {
+	return this.topSpeed;
+}
 
 // Do not change the code below
 window.Car = Car;
